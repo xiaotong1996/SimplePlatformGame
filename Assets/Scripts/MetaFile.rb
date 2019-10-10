04d06466c2d34a5f5b05593d0f83912b14e997a5
xiaotong1996 <chen.xiaotong@hotmail.com>
2019-10-10 16:12:11 +0200

    basic functions

---
+++ Assets/Scripts/MetaFile.rb
@@ -0,0 +1,15 @@
+﻿
+require 'find'
+ 
+metafiles = []
+Find.find(Dir.getwd()) do |path|
+    metafiles << path if path =~ /.*\.meta$/
+end
+
+metafiles.each do |fileloc|
+    file = File.open(fileloc)
+    line = nil
+    2.times{ line = file.gets}
+puts fileloc if not line = ~ /.*:.*/
+    file.close
+end
---
+++ Assets/Scripts/PlatformAutoMove.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Class <c>PlatformAutoMove</c> represents platform move automatically form start point to end point.
+/// </summary>
+public class PlatformAutoMove : MonoBehaviour
+{
+
+    [SerializeField]
+    public Transform startPoint;
+    [SerializeField]
+    public Transform endPoint;
+    [SerializeField]
+    private float moveTime = 2f;
+    [SerializeField]
+    private float moveSpeed = 1f;
+    /// <summary>
+    /// check is Arrived at start point or end point  or not
+    /// </summary>
+    [SerializeField]
+    private bool isArrive = false; 
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        PlatformMove();
+    }
+
+    void PlatformMove()
+    {
+        if(!isArrive)
+        {
+            
+            transform.position = Vector3.MoveTowards(transform.position, endPoint.position, moveSpeed * Time.deltaTime);
+            if (transform.position == endPoint.position)
+                isArrive = true;
+        }
+        else
+        {
+            transform.position = Vector3.MoveTowards(transform.position, startPoint.position, moveSpeed * Time.deltaTime);
+            if (transform.position == startPoint.position)
+                isArrive = false;
+        }
+    }
+}
