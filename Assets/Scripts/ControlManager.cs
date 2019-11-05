5d20255c33f765a19fc31caa8264ce43cb03a87c
xiaotong1996 <chen.xiaotong@hotmail.com>
2019-11-05 22:54:53 +0100

    add control panel

---
+++ Assets/Scripts/ControlManager.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ControlManager : MonoBehaviour
+{
+    private int arrowPos = -95;
+   // public 
+    private RectTransform arrowTransform;
+    private int currentChoice = 1;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        arrowTransform=GetComponent<RectTransform>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButtonUp("Vertical"))
+        {
+            Debug.Log(arrowTransform.anchoredPosition);
+            if (currentChoice == 1)
+            {
+                arrowTransform.anchoredPosition += new Vector2(0,arrowPos);
+                currentChoice = 2;
+
+                Debug.Log(arrowTransform.anchoredPosition);
+            }
+            else if (currentChoice == 2)
+            {
+                arrowTransform.anchoredPosition -= new Vector2(0, arrowPos);
+                currentChoice = 1;
+                Debug.Log(arrowTransform.anchoredPosition);
+            }
+        }
+
+        if (Input.GetAxisRaw("Jump") == 1)
+        {
+            if (currentChoice == 1)
+            {
+                LoadLevel("home");
+            }
+            else if (currentChoice == 2)
+            {
+                Quit();
+            }
+
+        }
+       
+    }
+
+    public void LoadLevel(string name)
+    {
+
+        SceneManager.LoadScene(name);
+    }
+
+
+
+    public void Quit()
+    {
+#if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+#else
+        Application.Quit();
+#endif
+    }
+}
---
+++ Assets/Scripts/ControlPanel.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ControlPanel : MonoBehaviour
+{
+    GameObject controlPanel;
+    private void Awake()
+    {
+        controlPanel = (GameObject)Instantiate(Resources.Load("Prefabs/ControlPanel"));
+        controlPanel.SetActive(false);
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButtonDown("Setting"))
+        {
+            if (controlPanel.activeSelf)
+            {
+                controlPanel.SetActive(false);
+            }
+            else
+            {
+                controlPanel.SetActive(true);
+            }
+            
+        }
+    }
+}
